6bb8431cc811d096254aa53e07f2c65947521622
farleybsd <farley.t.i@hotmail.com>
2025-05-12 17:12:29 -0300

    .
    .

---
+++ src/Application/Abstractions/Messaging/ICommand.cs
@@ -0,0 +1,9 @@
+﻿using SharedKernel;
+
+namespace Application.Abstractions.Messaging;
+
+public interface ICommand : ICommand<Result>;
+
+public interface ICommand<TResponse> : IBaseCommand;
+
+public interface IBaseCommand;
---
+++ src/Application/DependencyInjection.cs
@@ -0,0 +1,45 @@
+﻿using Application.Abstractions.Behaviors;
+using Application.Abstractions.Messaging;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using SharedKernel;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        // Register custom IQueryHandler and ICommandHandler
+        services.Scan(scan => scan
+            .FromAssembliesOf(typeof(DependencyInjection))
+            .AddClasses(classes => classes.AssignableTo(typeof(IQueryHandler<,>)), publicOnly: false)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+
+        services.Scan(scan => scan
+            .FromAssembliesOf(typeof(DependencyInjection))
+            .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<>)), publicOnly: false)
+                .AsImplementedInterfaces()
+            .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<,>)), publicOnly: false)
+                .AsImplementedInterfaces()
+            .WithScopedLifetime());
+
+        services.Decorate(typeof(ICommandHandler<,>), typeof(ValidationDecorator.CommandHandler<,>));
+        services.Decorate(typeof(ICommandHandler<>), typeof(ValidationDecorator.CommandBaseHandler<>));
+
+        services.Decorate(typeof(IQueryHandler<,>), typeof(LoggingDecorator.QueryHandler<,>));
+        services.Decorate(typeof(ICommandHandler<,>), typeof(LoggingDecorator.CommandHandler<,>));
+        services.Decorate(typeof(ICommandHandler<>), typeof(LoggingDecorator.CommandBaseHandler<>));
+
+        services.Scan(scan => scan
+            .FromAssembliesOf(typeof(DependencyInjection))
+            .AddClasses(classes => classes.AssignableTo(typeof(IDomainEventHandler<>)), publicOnly: false)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly, includeInternalTypes: true);
+
+        return services;
+    }
+}
